2e52e8aed1617f2803e0655ef5ebe723f680e4c3
Arina <hadson.rey@mail.ru>
2021-05-06 13:37:44 +0300

    Добавляет логику поиска подстроки (wip)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,26 +2,61 @@ import React, {PureComponent} from "react";
 import {ListGroup} from "../listGroup/listGroup";
 import {Favorites} from "../favorites/favorites";
 import {User} from "../../types/user";
+import {Search} from "../search/search";
 
 interface AppProps {
-    users: Array<User>
+    users: Array<User>;
 }
 
 interface AppState {
-    initialFavoriteUsers: []
+    favoriteUsers: Array<User>;
+    searchValue: string;
 }
 
 export class App extends PureComponent<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = {
+            favoriteUsers: [],
+            searchValue: ""
+        }
+
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+    }
+
+    private handleSearchChange(value: string) {
+        this.setState({
+           searchValue: value
+        });
+    }
+
+    private getFoundUsers() {
+        const {users} = this.props;
+        const {searchValue} = this.state;
+
+        if (searchValue.length === 0) {
+            return users;
+        }
+
+        return users.filter((user) => {
+            const {name: {title, first, last}} = user;
+            const joined = `${title} ${first} ${last}`;
+
+            return joined.includes(searchValue);
+        });
+    }
 
     render() {
-        const {users} = this.props;
+        const users = this.getFoundUsers();
+        const {favoriteUsers, searchValue} = this.state;
 
         return (
             <section className="app">
                 <h1 className="app__title">Список пользователей</h1>
-
+                <Search value={searchValue} onChange={this.handleSearchChange} />
                 <ListGroup users={users} />
-                <Favorites favoriteUsers={[]} />
+                <Favorites favoriteUsers={favoriteUsers} />
             </section>
         )
     }
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,11 +1,37 @@
-import React, {PureComponent} from "react";
+import React, {ChangeEvent, PureComponent} from "react";
 
-export class Search extends PureComponent {
+interface SearchProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export class Search extends PureComponent<SearchProps> {
+    constructor(props: SearchProps) {
+        super(props);
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    private handleInputChange(evt: ChangeEvent<HTMLInputElement>) {
+        const {target: {value}} = evt;
+        const {onChange} = this.props;
+
+        onChange(value);
+    }
+
     render() {
+        const {value} = this.props;
+
         return (
             <label className="list-group__search search" htmlFor="search">
                 Поиск
-                <input className="search__input" type="text" id="search"/>
+                <input
+                    value={value}
+                    className="search__input"
+                    type="text"
+                    id="search"
+                    onChange={this.handleInputChange}
+                />
             </label>
         );
     }
