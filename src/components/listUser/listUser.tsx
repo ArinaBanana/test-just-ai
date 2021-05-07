a822ec61cb30b398c5db2bcaf11a4992f642a667
Arina <hadson.rey@mail.ru>
2021-05-07 16:13:24 +0300

    Добавляет поддержку частичного показа пользователей

--- src/components/listUser/listUser.tsx
+++ src/components/listUser/listUser.tsx
@@ -6,19 +6,64 @@ interface ListUserProps {
     users: Array<IUser>;
 }
 
-export class ListUser extends PureComponent<ListUserProps> {
+interface ListUserState {
+    count: number;
+}
+
+const SHOW_USERS_COUNT = 10;
+
+export class ListUser extends PureComponent<ListUserProps, ListUserState> {
+    constructor(props: ListUserProps) {
+        super(props);
+
+        this.state = {
+            count: SHOW_USERS_COUNT
+        }
+
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    private getShowedUsers() {
+        const {users} = this.props;
+        const {count} = this.state;
+
+        return users.slice(0, count);
+    }
+
+    private handleButtonClick() {
+        this.setState((prevState) => {
+            const {count: prevCount} = prevState;
+
+            return {
+                count: prevCount + SHOW_USERS_COUNT
+            }
+        })
+    }
+
     render() {
         const {users} = this.props;
+        const {count} = this.state;
+
+        const showedUsers = this.getShowedUsers();
+        const showedCount = users.length - count;
 
         return (
             <div className="list-user">
                 {
-                    users.map((user, index) => (
+                    showedUsers.map((user, index) => (
                        <User
                            key={`${user.id}`}
                            {...user}
                        />
                     ))
+                }
+
+                {
+                    showedCount > 0
+                    &&
+                    <button type="button" onClick={this.handleButtonClick}>
+                        Еще {showedCount < SHOW_USERS_COUNT ? showedCount : SHOW_USERS_COUNT} пользователей
+                    </button>
                 }
             </div>
         );
